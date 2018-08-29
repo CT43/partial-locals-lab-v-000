59f9e1d0ba78154bed17480537a098d1e96c25d6
Christian Thomas <christian.thomas.2894@gmail.com>
2018-08-29 21:38:21 +0000

    automatically backed up by learn

--- app/controllers/classrooms_controller.rb
+++ app/controllers/classrooms_controller.rb
@@ -1,7 +1,7 @@
 class ClassroomsController < ApplicationController
   def show
     @classroom = Classroom.find(params[:id])
-    @students = @classroom.students
+    @student = @classroom.students
   end
 
   def index
